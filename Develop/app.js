617977458c4a6879ef88ba85b2133906c7eb0593
Jacob Hendershott <jwhendershott@gmail.com>
2021-01-27 19:08:40 -0600

    added intern prompts and case

--- Develop/app.js
+++ Develop/app.js
@@ -10,26 +10,142 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+async function managerPrompts() {
+var test = await inquirer
+    .prompt ([
+        {
+            type: 'input',
+            name: 'mangName',
+            message: "What is your manager's name?",
+        },
+        {
+            type: 'input',
+            name: 'empID',
+            message: 'What is your employee ID?',
+        },
+        {
+            type: 'input',
+            name: 'officeNo',
+            message: 'What is your office phone number?',
+        },
+        {
+            type: 'input',
+            name: 'mangEmail',
+            message: 'What is your email address?',
+        },
+    ])
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+    .then(async function (manager) {
+        var memArr = [];
+        while(memArr.length == 0 || memArr [memArr.length - 1].type !== 'I am done adding members.' ) {
+            memArr.push(await addToTeam());
+        }
+        memArr.pop();
+        return {manager: manager, members: memArr}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+        
+    });
+    console.log(test);
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+managerPrompts();
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+async function addToTeam () {
+    var teamMember
+    return await inquirer.prompt([
+    {
+        type: 'list',
+        name: 'memberChoice',
+        message: 'Who would you like to add to your team?',
+        choices: ['engineer', 'intern', 'I am done adding members.'],
+    }
+    ])
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    .then(async function (data) {
+        console.log(data);
+        switch (data.memberChoice){
+            case 'engineer':
+                var engBro = await engineerPrompts();
+                teamMember = {type: 'engineer', 
+                name: engBro.engName, 
+                id: engBro.engEmpID,
+                email: engBro.engEmail,
+                github: engBro.engGithub}
+                break;
+            case 'intern': 
+            var internBro = await internPrompts();
+            teamMember = {type: 'intern',
+            name: internBro.internName,
+            id: internBro.internEmpID,
+            email: internBro.school,
+            github: internBro.internGithub}
+                break;
+            case 'I am done adding members.':
+                teamMember = {type: 'I am done adding members.'}
+                break;
+            
+        }  
+    return teamMember;
+    });
+}
+
+async function engineerPrompts() {
+    return await inquirer
+        .prompt ([
+            {
+                type: 'input',
+                name: 'engName',
+                message: "What is your name?",
+            },
+            {
+                type: 'input',
+                name: 'engEmpID',
+                message: 'What is your employee ID?',
+            },
+            {
+                type: 'input',
+                name: 'engEmail',
+                message: 'What is your email address?',
+            },
+            {
+                type: 'input',
+                name: 'engGithub',
+                message: 'What is your GitHub username?',
+            },
+        ])
+
+    .then (data => {
+        return data;
+    });
+}
+
+async function internPrompts() {
+    return await inquirer
+        .prompt ([
+            {
+                type: 'input',
+                name: 'internName',
+                message: "What is your name?",
+            },
+            {
+                type: 'input',
+                name: 'internEmpID',
+                message: 'What is your employee ID?',
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What school do you go to?',
+            },
+            {
+                type: 'input',
+                name: 'internGithub',
+                message: 'What is your GitHub username?',
+            },
+        ])
+
+    .then (data => {
+        return data;
+    });
+}
+
